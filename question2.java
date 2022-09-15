f5586da80fbf644a51406108c21eb66e0b2ba9df
Amartya Sharma <62010611+Amartya-mma@users.noreply.github.com>
2022-09-15 11:47:18 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,15 @@
+public class question2{
+    public static void main(String[] args){
+    
+       float c = 2.02f;
+        float d = 3.03f;
+
+        float mul = c*d;
+
+
+        System.out.println("ques 2:");
+        System.out.println("c: " + c + ", d: " + d);
+        System.out.println("c * d: " + mul);
+
+    }
+}
